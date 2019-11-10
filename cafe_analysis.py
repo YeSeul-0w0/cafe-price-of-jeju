5ea061f497988b0d08f5827113ac38060d0ead41
seoyl98 <seoyla98@gmail.com>
2019-11-10 14:31:21 +0900

    한글폰트 적용해보기
    9-2)참고

---
+++ cafe_analysis.py
@@ -0,0 +1,20 @@
+import numpy as np # 수학 연산 수행을 위한 모듈
+import pandas as pd # 데이터 처리를 위한 모듈
+import seaborn as sns # 데이터 시각화 모듈
+import matplotlib.pyplot as plt # 데이터 시각화 모듈
+import matplotlib as mpl
+import matplotlib.font_manager as fm
+
+mpl.rcParams['axes.unicode_minus'] = False
+
+data = np.random.randint(-100, 100, 50).cumsum()
+
+plt.rcParams["font.family"] = 'NanumGothic'
+plt.rcParams["font.size"] = 20
+plt.rcParams["figure.figsize"] = (14,4)
+
+plt.plot(range(50), data, 'r')
+plt.title('시간별 가격 추이')
+plt.ylabel('주식 가격')
+plt.xlabel('시간(분)')
+plt.show()
