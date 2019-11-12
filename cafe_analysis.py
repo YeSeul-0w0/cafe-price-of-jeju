0fa6f8469d34e80fa62d7ad3b243a47bd740ecd0
art3choi <rena258@naver.com>
2019-11-12 16:03:19 +0900

    경도 위도 plot 출력

--- cafe_analysis.py
+++ cafe_analysis.py
@@ -1,11 +1,11 @@
-import numpy as np # 수학 연산 수행을 위한 모듈
-import pandas as pd # 데이터 처리를 위한 모듈
-import seaborn as sns # 데이터 시각화 모듈
-import matplotlib.pyplot as plt # 데이터 시각화 모듈
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
 import matplotlib as mpl
 import matplotlib.font_manager as fm
-import folium
-import warnings
+import folium # 지도 사용시 필요한 모듈듈
+imort warnings
 warnings.filterwarnings('ignore')
 warnings.filterwarnings('ignore', 'This pattern has match groups')
 warnings.filterwarnings('ignore', 'The iterable function was deprecated in Matplotlib')
@@ -18,11 +18,8 @@ plt.rcParams["font.family"] = 'Malgun Gothic'
 plt.rcParams["font.size"] = 15
 plt.rcParams["figure.figsize"] = (14,4)
 
-'''
 data[['위도', '경도']].describe(include=np.number)
 data['위도']=data['위도'].astype(float)
 data['경도']=data['경도'].astype(float)
-data.plot.scatter(x='경도', y='위도')
-'''
-
-print(data.info())
+data.plot.scatter(x='경도', y='위도') #위도 경도 값 받아서 표현
+plt.show() #show함수로 받은 위도 경도 값 출력
