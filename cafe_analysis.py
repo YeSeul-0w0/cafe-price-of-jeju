315ab2973a3e4b8c883f15f0d0253aceba91fcd4
seoyl98 <seoyla98@gmail.com>
2019-11-16 17:41:40 +0900

    색 팔레트 지정 및 주변 카페 수 csv 추가

--- cafe_analysis.py
+++ cafe_analysis.py
@@ -22,17 +22,7 @@ data[['위도', '경도']].describe(include=np.number)
 data['위도']=data['위도'].astype(float)
 data['경도']=data['경도'].astype(float)
 
-
-
-sns.relplot(data=data, x="경도",y="위도",hue="동")
-
-'''
-plt.scatter(data.경도,data.위도,alpha=0.2,s=30, c=data.위도, cmap='viridis') 
-(x값, y값, alpha는 뭔지 모르겠고 s는 동그라미 사이즈 c는 색상 cmap은 뭔지 모름)
-plt.colorbar() #옆에 색 bar 뜨는거
-'''
+sns.relplot(data=data, x="경도", y="위도", hue="동", palette=sns.color_palette("colorblind", 19)) #sns.color_palette("팔레트 이름", 색 수)
 
 plt.title('위도, 경도별 카페 분포',fontsize=20) #타이틀
 plt.show() #show함수로 받은 위도 경도 갑
-
-#지금 한 건 그 동 별로 묶어서 색깔 다르게 만든거 주석처리한건 그 위도인가 경도인가 그거 기준으로 색 다르게 출력되는거.
--- haversiine.py
+++ haversiine.py
@@ -1,3 +1,4 @@
+#주변 카페 수 세기
 from haversine import haversine
 import pandas as pd
 
@@ -8,6 +9,6 @@ for i in data.index:
     for j in data.index:
         point1 = ( data.loc[i,'위도'], data.loc[i, '경도']) #기준점
         point2 = ( data.loc[j,'위도'], data.loc[j, '경도']) #비교할위치
-        if haversine(point1,point2) <= 0.5: #거리 구해서 비교 / 1=1km 0.5=500m
+        if haversine(point1,point2) <= 0.25: #거리 구해서 비교 / 1=1km 0.5=500m
             count = count+1
-    print(i, count)
+    print(count)
