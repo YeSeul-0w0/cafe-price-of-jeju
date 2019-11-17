3960102331c9d64e1e9d2e939e9ff4714ff464ac
Choi33 <choi333ys@gmail.com>
2019-11-17 15:59:11 +0900

    지도에 동별 marker 표시

--- cafe_analysis.py
+++ cafe_analysis.py
@@ -29,21 +29,62 @@ sns.relplot(data=data, x="경도", y="위도", hue="동", palette=sns.color_pale
 plt.title('위도, 경도별 카페 분포',fontsize=20) #타이틀
 # plt.show() #show함수로 받은 위도 경도 갑
 
-dong_one=data.loc[data['동']==1]  #동 번호 1인 데이터만 저장
 
-map_one_alt=[];
-map_one_long=[];
+map_alt=[];
+map_long=[];
 
-for i in range(len(dong_one)):
-    map_one_alt.append(dong_one['위도'].iloc[i])
-    map_one_long.append(dong_one['경도'].iloc[i])
+for i in range(len(data)):
+    map_alt.append(data['위도'].iloc[i])
+    map_long.append(data['경도'].iloc[i])
 
-map_one=folium.Map(location=[dong_one['위도'].iloc[0],dong_one['경도'].iloc[0]],zoom_start=13)
 
-for i in range(len(dong_one)):
+map_one=folium.Map(location=[data['위도'].iloc[0],data['경도'].iloc[0]],zoom_start=13)
+
+for i in range(len(data)):
     # map_one=folium.Map(location=[map_one_alt[i],map_one_long[i]],zoom_start=13)
-    folium.CircleMarker([map_one_alt[i],map_one_long[i]],radius=7,popup=i, color='#123456', fill_color='#123456').add_to(map_one)
+    if(data['동'].iloc[i]==1):
+        folium.CircleMarker([map_alt[i],map_long[i]],radius=7,popup=i, color='#FF0000', fill_color='##FF0000').add_to(map_one)
+    if(data['동'].iloc[i]==2):
+        folium.CircleMarker([map_alt[i],map_long[i]],radius=7,popup=i, color='#F29661', fill_color='#F29661').add_to(map_one)
+    if(data['동'].iloc[i]==3):
+        folium.CircleMarker([map_alt[i],map_long[i]],radius=7,popup=i, color='#980000', fill_color='#980000').add_to(map_one)
+    if(data['동'].iloc[i]==4):
+        folium.CircleMarker([map_alt[i],map_long[i]],radius=7,popup=i, color='#FFBB00', fill_color='#FFBB00').add_to(map_one)
+    if(data['동'].iloc[i]==5):
+        folium.CircleMarker([map_alt[i],map_long[i]],radius=7,popup=i, color='#E5D85C', fill_color='#E5D85C').add_to(map_one)
+    if(data['동'].iloc[i]==6):
+        folium.CircleMarker([map_alt[i],map_long[i]],radius=7,popup=i, color='#997000', fill_color='#997000').add_to(map_one)
+    if(data['동'].iloc[i]==7):
+        folium.CircleMarker([map_alt[i],map_long[i]],radius=7,popup=i, color='#1F3456', fill_color='#1F3456').add_to(map_one)
+    if(data['동'].iloc[i]==8):
+        folium.CircleMarker([map_alt[i],map_long[i]],radius=7,popup=i, color='#ABF200', fill_color='#ABF200').add_to(map_one)
+    if(data['동'].iloc[i]==9):
+        folium.CircleMarker([map_alt[i],map_long[i]],radius=7,popup=i, color='#86E57F', fill_color='#86E57F').add_to(map_one)
+    if(data['동'].iloc[i]==10):
+        folium.CircleMarker([map_alt[i],map_long[i]],radius=7,popup=i, color='#6B9900', fill_color='#6B9900').add_to(map_one)
+    if(data['동'].iloc[i]==11):
+        folium.CircleMarker([map_alt[i],map_long[i]],radius=7,popup=i, color='#00D8FF', fill_color='#00D8FF').add_to(map_one)
+    if(data['동'].iloc[i]==12):
+        folium.CircleMarker([map_alt[i],map_long[i]],radius=7,popup=i, color='#6799FF', fill_color='#6799FF').add_to(map_one)
+    if(data['동'].iloc[i]==13):
+        folium.CircleMarker([map_alt[i],map_long[i]],radius=7,popup=i, color='#008299', fill_color='#008299').add_to(map_one)
+    if(data['동'].iloc[i]==14):
+        folium.CircleMarker([map_alt[i],map_long[i]],radius=7,popup=i, color='#0100FF', fill_color='#0100FF').add_to(map_one)
+    if(data['동'].iloc[i]==15):
+        folium.CircleMarker([map_alt[i],map_long[i]],radius=7,popup=i, color='#A566FF', fill_color='#A566FF').add_to(map_one)
+    if(data['동'].iloc[i]==16):
+        folium.CircleMarker([map_alt[i],map_long[i]],radius=7,popup=i, color='#050099', fill_color='#050099').add_to(map_one)
+    if(data['동'].iloc[i]==17):
+        folium.CircleMarker([map_alt[i],map_long[i]],radius=7,popup=i, color='#FF00DD', fill_color='#FF00DD').add_to(map_one)
+    if(data['동'].iloc[i]==18):
+        folium.CircleMarker([map_alt[i],map_long[i]],radius=7,popup=i, color='#F361A6', fill_color='#F361A6').add_to(map_one)
+    if(data['동'].iloc[i]==19):
+        folium.CircleMarker([map_alt[i],map_long[i]],radius=7,popup=i, color='#990085', fill_color='#990085').add_to(map_one)
+
+
+
+
 
 map_one.save('dong_one.html',encoding='utf-8')
 
-print(map_one_alt[15])
+# print(map_one_alt[15])
